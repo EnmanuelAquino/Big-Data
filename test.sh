5f8fc2918665e7697b720b5dfc9fbf92d2d98c62
EnmanuelAquino <126352152+EnmanuelAquino@users.noreply.github.com>
2023-02-25 15:50:01 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 /usr/local/hadoop/bin/hdfs dfs -rm -r /part1/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /part1/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /part1/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal access.log /part1/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../data/access.log /part1/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
 -file mapper.py -mapper mapper.py \
 -file reducer.py -reducer reducer.py \
